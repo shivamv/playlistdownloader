0e3f02c6a173eba42c108cb9f20fdf9ba3ee7167
Shivam Verma <shvamverma@gmail.com>
2016-03-08 14:47:38 +0530

    [Shivam] Changes to install

--- install.py
+++ install.py
@@ -1,12 +1,18 @@
 import os
-os.system("sudo apt-get install python-celery")
-from celery import chord
+import threading
+from threading import Thread
+def install_celery():
+  os.system("sudo apt-get install python-celery")
+  from celery import chord
 task1=os.system("sudo add-apt-repository ppa:nilarimogard/webupd8")
 task2=os.system("sudo apt-get update")
 task3=os.system("sudo apt-get install youtube-dl")
 task4=os.system("sudo apt-get install gnome-panel")
 task5=os.system("sudo gnome-desktop-item-edit --create-new ~/Desktop")
 def callback():
-  result=chord(task6)
+  os.system("sudo apt-get install python-tk")
+  result=chord(task1,task2,task3,task4,task5)
   result.get()
+thread1 = Thread(target = install_celery, args = ())
+thread1.start
 callback()
